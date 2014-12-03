6ab1526c8e83e32a0086dc7d1d3649a82125e5f6
Scotty Jacobson <ScottyMJacobson@gmail.com>
2014-12-02 20:22:52 -0500

    Made introdemo chart a pie chart

--- introdemo/priv/assets/js/index.js
+++ introdemo/priv/assets/js/index.js
@@ -1,186 +1,78 @@
 
-function startGame(ostracon) {
-  //totals represent total keystrokes, 
-  //states represent the keystroke frequency of the most recent state
-  var leftTotal = 0;
-  var leftState = 0;
-  var rightTotal = 0;
-  var rightState = 0;
-  var upTotal = 0;
-  var upState = 0;
-  var downTotal = 0;
-  var downState = 0;
-  // Load the Visualization API and the piechart package.
-  google.load('visualization', '1.1', {'packages':['bar']});
+function createChart(ostracon) {
 
-  // Set a callback to run when the Google Visualization API is loaded.
+  google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(runTableLoop);
 
-  google.setOnLoadCallback(init);
+    //totals represent total keystrokes, 
+  var totalsArray = [['Keystroke', 'Total'], 
+                      ['Left', 10], 
+                      ['Right', 1], 
+                      ['Up', 1], 
+                      ['Down', 9]];
 
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function init() {
+  var currentData;
+
+  function runTableLoop() {
     // Create the data table.
-    data = new google.visualization.arrayToDataTable([['Keystroke', 'Total', 'Recent State'], 
-                      ['Left', leftTotal, leftState], 
-                      ['Right', rightTotal, rightState], 
-                      ['Up', upTotal, upState], 
-                      ['Down', downTotal, downState]])
-    
-    function tableValues() {
-      return ([['Keystroke', 'Total', 'Recent State'], 
-                      ['Left', leftTotal, leftState], 
-                      ['Right', rightTotal, rightState], 
-                      ['Up', upTotal, upState], 
-                      ['Down', downTotal, downState]])
-    };
+    currentData = google.visualization.arrayToDataTable(totalsArray);
 
     // Set chart options
-
     var options = { 
-      'width':900,
-      chart: {
-        title:'Ostracon Visualization',
-        subtitle: 'total on left, last state on the right'
-      }, 
-      series: {
-        0: {axis: 'Total'}, 
-        1: {axis: 'Recent State'}
-      }, 
-      axes: {
-        y: {
-          'Total': {label: 'votes'},
-          'Recent State': {side: 'right', label: 'votes'}
-        }
-      }
+       width:900,
+       title:'Ostracon Visualization'
     };
 
-
     // Instantiate and draw our chart, passing in some options.
-    var chart = new google.charts.Bar(document.getElementById('chart_div'));
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
 
-     function drawChart() {
-       chart.draw(data, options);
-     }
-     drawChart();
+    var chartState;
 
-    setInterval(function(){updateOstracon()}, 1000);
-    
-    function updateOstracon() {
+    chart.draw(currentData, options);
+
+    function chartStateToArray (chartState) {
+      leftTotal = chartState["left"];
+      rightTotal = chartState["right"];
+      upTotal = chartState["up"];
+      downTotal = chartState["down"];
+      return [['Keystroke', 'Total'], 
+                      ['Left', leftTotal], 
+                      ['Right', rightTotal], 
+                      ['Up', upTotal], 
+                      ['Down', downTotal]];
+    }
+
+    setInterval(function(){updateChart()}, 200);
+
+    function isStateDifference(state1, state2) {
+      if ((typeof state1 == "undefined") || (typeof state2 == "undefined")) {
+        return true;
+      } 
+      isDifferent = false;
+      for (key in state1) {
+        isDifferent = isDifferent || state1[key] != state2[key];
+      } 
+      return isDifferent;
+    }
+
+    function updateChart() {
       ostracon.requestState();
-      gameState = ostracon.getState();
-      console.log(gameState)
-      if (gameState) { 
-        leftState = gameState["left"] - leftTotal;
-        rightState = gameState["right"] - rightTotal;
-        upState = gameState["up"] - upTotal;
-        downState = gameState["down"] - downTotal;
-
-	      leftTotal = gameState["left"];
-	      rightTotal = gameState["right"];
-	      upTotal = gameState["up"];
-	      downTotal = gameState["down"];
-
-	      console.log(tableValues())
-	      data = google.visualization.arrayToDataTable(tableValues());
-
-	      drawChart();
-	    }
+      var newChartState = ostracon.getState();
+      if (newChartState && isStateDifference(newChartState, chartState)) { 
+        chartState = newChartState;
+        currentData = google.visualization.arrayToDataTable(chartStateToArray(chartState));
+        chart.draw(currentData, options);
+      }
     }
   }
 }
 
 
-
-function Ostracon () {
-    ostracon = this;
-
-    ostracon.start = function() {
-        ostracon.initWebSocket();
-    };
-
-    ostracon.initWebSocket = function() {
-        if (!("WebSocket" in window)) {
-            alert("Your browser doesn't support WebSockets! Try Chrome :P");
-            return;
-        }
-        console.log("beginning connection:");
-        ostracon.ws = new WebSocket("ws://" + window.location.host + "/websocket");
-
-        ostracon.ws.onopen = function () {
-            console.log("Connection started...");
-            ostracon.requestState();
-            ostracon.open = true;
-        };
-        ostracon.ws.onmessage = ostracon.handleMessage;
-        ostracon.ws.onclose = ostracon.handleWSClose;
-
-    };
-
-    ostracon.handleWSClose = function() {
-        console.log("WebSocket connection killed.");
-        ostracon.open = false;
-        ostracon.ws = null;
-        setTimeout(function() {
-            console.log("Attempting reconnect.");
-            ostracon.initWebSocket();
-        }, 10*1000);
-    };
-
-    ostracon.getState = function() {
-    		//alert(ostracon.state)
-        return ostracon.state;
-    };
-
-    ostracon.formatVote = function (voteText) {
-        var voteObject = {
-            type: "vote",
-            vote: voteText,
-            team: 'myTeam'
-        };
-        return voteObject;
-    };
-
-    ostracon.pushVote= function(voteInfo) {
-        if (ostracon.ws && ostracon.open) {
-        		//add sound effect here
-        		alert("pushin yo vote baby")
-            ostracon.ws.send(JSON.stringify(voteInfo));
-        }
-    };
-
-    ostracon.handleMessage = function(msg) {
-        var parsed_message = JSON.parse(msg.data);
-
-        switch(parsed_message.type) {
-            case "stateresponse":
-        				console.log(Array.isArray(parsed_message.response))
-        				console.log(parsed_message.response)
-                ostracon.state = parsed_message.response;
-                break;
-            case "voteresponse":
-                break;
-            default:
-                break;
-        }
-    };
-
-    ostracon.requestState = function() {
-        var requestObject = {
-            type: 'statequery'
-        };
-        if (ostracon.ws && ostracon.open) {
-            ostracon.ws.send(JSON.stringify(requestObject));
-        }
-    };
-
-    ostracon.start();
-}
-
-
-
-
+$(document).ready(function() {
+    var ostracon = new Ostracon();
+    createChart(ostracon);
+    makeKeystrokeHandler(ostracon);
+});
 
 
 function makeKeystrokeHandler(ostracon) {
@@ -207,11 +99,3 @@ function makeKeystrokeHandler(ostracon) {
         e.preventDefault(); // prevent the default action (scroll / move caret)
     });
 }
-
-		// alert("you are being alerted beep boop")
-		// var text = document.getElementById('chart_div')
-  //   text.innerHTML = "<p>this is a test, beep boop</p>"
-    var ostracon = new Ostracon();
-    //var ostracon = 1
-    startGame(ostracon);
-    makeKeystrokeHandler(ostracon);
